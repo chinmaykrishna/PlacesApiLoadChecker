597cbab041a3edabb31adfe7a91030f1c4d8d9b0
chinmaykrishna <seekay3@gmail.com>
2015-01-26 20:16:44 +0530

    Delete Constants.java

--- src/com/test/placesapiloadchecker/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.test.placesapiloadchecker;
-
-public class Constants {
-	public static final String apikey1="AIzaSyC4KZG7HFvHYCjMfeLsE_vNW3AsOp_vubY";
-	public static final String apikey2="AIzaSyCtO2knQl51Fpmq0XlxA7LTnuJZTZw6CEE";
-}
